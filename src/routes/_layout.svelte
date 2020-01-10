61aec3bcbf574a5353dc8b4203a9ebd53f4168e3
bublik <bublik.script@gmail.com>
2020-01-10 14:45:24 +0200

    style: Changed theme style in manifest.json

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,6 +3,17 @@
 	import Icons from './_icons.svelte';
 
 	export let segment;
+
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log('init')
+		window.addEventListener('popstate', function(event) {
+			console.log('works hide', event)
+			alert('works hide')
+		});
+	});
+
 </script>
 
 <style>
