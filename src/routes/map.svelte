cd6c191567ee5313fdafa6dbf82a3b5bd4bf49ef
bublik <bublik.script@gmail.com>
2020-02-02 23:27:54 +0200

    refactor: Optimized map load.

--- src/routes/map.svelte
+++ src/routes/map.svelte
@@ -13,53 +13,55 @@
 
     let map
     let container
-    let scriptTag
-    let link
 
-    // See example: https://github.com/ccd-adc-dev/sapper-mapbox/tree/master/src/components/map
+    function renderMap() {
+        map = new mapboxgl.Map({
+            container,
+            style: 'mapbox://styles/mapbox/streets-v11',
+        })
 
-    onMount(async () => {
-        // const { default: mapboxgl } = await import('mapbox-gl')
+        for (let i = 0; i < 50; i += 1) {
+            const lng = Math.random() * 360 - 180
+            const lat = Math.random() * 180 - 90
 
-        scriptTag = document.createElement('script')
+            new mapboxgl.Marker()
+                    .setLngLat([lng, lat])
+                    .addTo(map)
+        }
+        return map
+    }
+
+    function createMap() {
+        const scriptTag = document.createElement('script')
         scriptTag.type = 'text/javascript'
         scriptTag.src = 'https://api.mapbox.com/mapbox-gl-js/v1.7.0/mapbox-gl.js'
 
-        link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = 'https://api.mapbox.com/mapbox-gl-js/v1.7.0/mapbox-gl.css';
+        const link = document.createElement('link')
+        link.rel = 'stylesheet'
+        link.href = 'https://api.mapbox.com/mapbox-gl-js/v1.7.0/mapbox-gl.css'
 
         scriptTag.onload = () => {
             const token = 'pk.eyJ1IjoiYnVibGlrIiwiYSI6ImNrNXpxdzgxbTAwNnczbGxweG0wcTV3cjAifQ.rt1peLjCQHZUkrM4AWz5Mw'
-
             mapboxgl.accessToken = token
 
-            link.onload = () => {
-                map = new mapboxgl.Map({
-                    container,
-                    style: 'mapbox://styles/mapbox/streets-v11',
-                })
+            link.onload = () => renderMap()
 
-                for (let i = 0; i < 100; i += 1) {
-                    const lng = Math.random() * 360 - 180
-                    const lat = Math.random() * 180 - 90
-
-                    new mapboxgl.Marker()
-                            .setLngLat([lng, lat])
-                            .addTo(map)
-                }
-            }
-
-            document.head.appendChild(link);
+            document.head.appendChild(link)
         }
 
-        document.body.appendChild(scriptTag);
+        document.body.appendChild(scriptTag)
+    }
+
+    onMount(() => {
+        if ('mapboxgl' in window) {
+            renderMap()
+        } else {
+            createMap()
+        }
     })
 
     onDestroy(() => {
-        map.remove();
-        link.parentNode.removeChild(link);
-        scriptTag.parentNode.removeChild(scriptTag);
+        map && map.remove()
     })
 </script>
 
