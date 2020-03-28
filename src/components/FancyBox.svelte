d6ee599a887bd39b004dc93bb42663e25c84c309
bublik <bublik.script@gmail.com>
2020-03-28 12:11:12 +0200

    wip: Scroll.

--- src/components/FancyBox.svelte
+++ src/components/FancyBox.svelte
@@ -121,7 +121,7 @@
         top: 0;
         left: 0;
         width: 100%;
-        height: 100vh;
+        height: 100%;
         display: flex;
         overflow: hidden;
         align-items: stretch;
