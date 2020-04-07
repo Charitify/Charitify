65c408867f0c122ff0acc3ff105dee45eed6c6d3
bublik <bublik.script@gmail.com>
2020-04-07 22:47:12 +0300

    wip: Scroll propagation.

--- src/routes/organizations/_LastNews.svelte
+++ src/routes/organizations/_LastNews.svelte
@@ -23,85 +23,13 @@
 
     let modalActive
     $: modalActive = safeGet(() => $modals['modal-last-news'].open)
-    $: onTriggerModal(modalActive)
 
     function onFancyClose() {
         bodyScroll.disableScroll(scroller)
     }
 
-    function onTriggerModal() {
-        if (typeof window !== 'undefined') {
-            if (modalActive) {
-                document.body.addEventListener('touchmove', preventScroll, { passive: false })
-            } else {
-                document.body.removeEventListener('touchmove', preventScroll, { passive: false })
-            }
-        }
-    }
-
-    function preventScroll(evt) {
-        //In this case, the default behavior is scrolling the body, which
-        //would result in an overflow.  Since we don't want that, we preventDefault.
-        if(!evt._isScroller) {
-            evt.preventDefault()
-        }
-    }
-       
-    $: stopScrollOverflow(scroller)
-
-    function stopScrollOverflow(el) {
-        if (!el) return
-        el.addEventListener('touchstart', function() {
-        const top = el.scrollTop
-        const totalScroll = el.scrollHeight
-        const currentScroll = top + el.offsetHeight
-    
-        //If we're at the top or the bottom of the containers
-        //scroll, push up or down one pixel.
-        //
-        //this prevents the scroll from "passing through" to
-        //the body.
-        if(top === 0) {
-            el.scrollTop = 1
-        } else if(currentScroll === totalScroll) {
-            el.scrollTop = top - 1
-        }
-        })
-    
-        el.addEventListener('touchmove', function(evt) {
-        //if the content is actually scrollable, i.e. the content is long enough
-        //that scrolling can occur
-        if(el.offsetHeight < el.scrollHeight)
-            evt._isScroller = true
-        })
-    }
-
     function detectScroll(el) {
-
-        function getPercentage(el) {
-            let height = el.clientHeight;
-            let scrollHeight = el.scrollHeight - height;
-            let scrollTop = el.scrollTop;
-            return scrollTop / scrollHeight;
-        }
-        
-        function controllScroll(el, e) {
-            e.stopPropagation()
-            const percentage = getPercentage(el)
-            if (percentage <= 0) {
-                el.scrollTop = 1
-                console.log('red line of scroller')
-            } else if (percentage >= 100) {
-                let height = el.clientHeight;
-                let scrollHeight = el.scrollHeight - height;
-                el.scrollTop = scrollHeight - 1
-                console.log('red line of scroller')
-            }
-            console.log(percentage)
-        }
-
-        el.addEventListener('touchmove', controllScroll.bind(null, el))
-        el.addEventListener('scroll', controllScroll.bind(null, el))
+        bodyScroll.disableScroll(el)
     }
 </script>
 
