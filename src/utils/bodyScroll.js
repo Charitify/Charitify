9b7f00ce8f1ecc78247bf3cea08eb71116dbdc4b
bublik <bublik.script@gmail.com>
2020-04-08 21:30:01 +0300

    wip: Scroll propagation.

--- src/utils/bodyScroll.js
+++ src/utils/bodyScroll.js
@@ -1,27 +1,26 @@
 export function disableScroll() {
-    const scrollDistance = document.documentElement.scrollTop
+    // const scrollDistance = document.documentElement.scrollTop
 
-    document.documentElement.classList.add('overflow-hidden')
-    document.body.classList.add('overflow-hidden')
-    document.body.addEventListener("touchmove", freezeVp, false);
-    
-    const vpH = window.innerHeight;
-    document.documentElement.style.height = vpH.toString() + "px";
-    document.body.style.height = vpH.toString() + "px";
+    document.body.classList.add('body-scroll-lock')
 
-    console.log(scrollDistance)
-    document.body.scrollTop = scrollDistance
+    // const vpH = window.innerHeight;
+    // document.documentElement.style.height = vpH.toString() + "px";
+    // document.body.style.height = vpH.toString() + "px";
+
+    // console.log(scrollDistance)
+    // document.body.scrollTop = scrollDistance
 }
 
 export function enableScroll() {
-    document.documentElement.style.height = null
-    document.body.style.height = null
+    document.body.classList.remove('body-scroll-lock')
+    // document.documentElement.style.height = null
+    // document.body.style.height = null
 
-    document.documentElement.classList.remove('overflow-hidden')
-    document.body.classList.remove('overflow-hidden')
-    document.body.removeEventListener("touchmove", freezeVp, false);
+    // document.documentElement.classList.remove('overflow-hidden')
+    // document.body.classList.remove('overflow-hidden')
+    // document.body.removeEventListener("touchmove", freezeVp, false);
 }
 
-function freezeVp(e) {
-    e.preventDefault();
-};
+// function freezeVp(e) {
+    // e.preventDefault();
+// };
