d5161036ee427d172df9a26c1efb879836b7e994
bublik <bublik.script@gmail.com>
2020-04-12 19:41:13 +0300

    wip: Swipe experiments.

--- src/utils/bodyScroll.js
+++ src/utils/bodyScroll.js
@@ -7,10 +7,10 @@ export function disableScroll(el) {
     // lastScrollPosition = document.documentElement.scrollTop
     document.body.classList.add('body-scroll-lock')
 
-    if (el) {
-        el.ontouchstart = (e) => (e.preventDefault(), e.stopPropagation());
-        el.ontouchmove = (e) => (e.preventDefault(), e.stopPropagation());
-        el.ontouchend = (e) => (e.preventDefault(), e.stopPropagation());
+    if (el && el.parentElement) {
+        el.parentElement.ontouchstart = (e) => (e.preventDefault(), e.stopPropagation());
+        el.parentElement.ontouchmove = (e) => (e.preventDefault(), e.stopPropagation());
+        el.parentElement.ontouchend = (e) => (e.preventDefault(), e.stopPropagation());
     }
     // document.body.style.position = `relative`
     // document.body.style.top = `-${lastScrollPosition}px`
@@ -19,10 +19,10 @@ export function disableScroll(el) {
 export function enableScroll(el) {
     document.body.classList.remove('body-scroll-lock')
 
-    if (el) {
-        el.ontouchstart = null;
-        el.ontouchmove = null;
-        el.ontouchend = null;
+    if (el && el.parentElement) {
+        el.parentElement.ontouchstart = null;
+        el.parentElement.ontouchmove = null;
+        el.parentElement.ontouchend = null;
     }
 //     // document.body.style.position = null
 //     // document.body.style.top = null
