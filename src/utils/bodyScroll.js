08b1f69e13b9a15c0d80d33704bd1a65146f25c9
bublik <bublik.script@gmail.com>
2020-04-12 19:03:47 +0300

    wip: Swipe experiments.

--- src/utils/bodyScroll.js
+++ src/utils/bodyScroll.js
@@ -4,15 +4,15 @@ export function disableScroll() {
     if (document.documentElement.classList.contains('body-scroll-lock')) {
         return
     }
-    lastScrollPosition = document.documentElement.scrollTop
-    document.documentElement.classList.add('body-scroll-lock')
-    document.body.style.position = `relative`
-    document.body.style.top = `-${lastScrollPosition}px`
+    // lastScrollPosition = document.documentElement.scrollTop
+    document.body.classList.add('body-scroll-lock')
+    // document.body.style.position = `relative`
+    // document.body.style.top = `-${lastScrollPosition}px`
 }
 
 export function enableScroll() {
-    document.documentElement.classList.remove('body-scroll-lock')
-    document.body.style.position = null
-    document.body.style.top = null
-    document.documentElement.scrollTop = lastScrollPosition
+    document.body.classList.remove('body-scroll-lock')
+    // document.body.style.position = null
+    // document.body.style.top = null
+    // document.documentElement.scrollTop = lastScrollPosition
 }
