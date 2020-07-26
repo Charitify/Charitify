e499ee73c0f40b39a18a18c74ba847b6d70f56ea
bublik <bublik.script@gmail.com>
2020-07-26 16:10:16 +0300

    Updates

--- client/client.45e07c2f.js
+++
@@ -1,1 +0,0 @@
-import"./client.90626ed7.js";
---
+++ client/index.b195ea22.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as e,as as n}from"./client.eff62fdd.js";function r(s){return n("lists/funds"),[]}export default class extends s{constructor(s){super(),t(this,s,r,null,e,{})}}
