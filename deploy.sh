626680110cd7bf1a478615ab47083ca8588d85ad
Dmytro005 <harazdovskiy@gmail.com>
2020-09-05 20:15:50 +0300

    Maybe fail 7

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,18 @@ rm -rf ./__sapper__
 git checkout maser
 git pull
 
-# echo "Installing packages"
-# npm install
+echo "Installing packages"
+npm install
 
-# echo "Exporting static files"
-# npm run export
+echo "Exporting static files"
+npm run export
 
-# echo "Building production code"
-# npm run build
+echo "Building production code"
+npm run build
 
-if pm2 -v; then
-    echo "pm2 is insalled"
-else
-    npm install pm2 -g
-fi
+echo "Checking pm2 version: "
+if !pm2 -v; then npm install pm2 -g
+    fi
 
 if pm2 start  __sapper__/build/index.js --name charitify ; then
     echo "Finish"
