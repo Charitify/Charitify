a35ee97f958e68bf5aabf56e11b969090d3ae333
Dmytro005 <harazdovskiy@gmail.com>
2020-09-05 19:29:20 +0300

    Maybe fail 2

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,13 @@ git pull
 npm run i
 npm run export
 npm run build
+
+if pm2 -v; then
+    echo "pm2 is insalled"
+else
+    npm install pm2 -g
+fi
+
 if pm2 start  __sapper__/build/index.js --name charitify ; then
     echo "Finish"
 else
