5561ddcfebd56e8e09340ea84ba487bdf78eb16f
Dmytro005 <harazdovskiy@gmail.com>
2020-09-06 04:48:31 +0300

    Good night 😴

--- deploy.sh
+++ deploy.sh
@@ -13,19 +13,21 @@ echo "Building production code"
 npm run build
 
 echo "Checking pm2 version: "
-if !pm2 -v; then npm install pm2 -g
-    fi
+if ! pm2 -v ; then
+    npm install pm2 -g
+fi
 
 echo "Checking sapper version: "
-if !sapper -v; then npm install sapper -g
-    fi
+if ! sapper -v ; then
+    npm install sapper -g
+fi
 
-if !pm2 start  __sapper__/build/index.js --name charitify ; then
+if ! pm2 start  __sapper__/build/index.js --name charitify ; then
     pm2 restart charitify --update-env
     echo "Finish"
 fi
 
 # If nginx config was changed use this command to restart hte server
-systemctl restart nginx
+# systemctl restart nginx
 
-exit 0
+# exit 0
